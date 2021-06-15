c4de718396c899e2e33588285ae8994b61e33654
LouisFaure <louis.faure75@gmail.com>
2021-06-15 23:50:03 +0200

    rewording to lower case

--- dyntrack/plot/fit_ppt.py
+++ dyntrack/plot/fit_ppt.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import SimplePPT
+import simpleppt
 
 
 def fit_ppt(img,ppts,tdata,times=None,**kwargs):  
@@ -10,7 +10,7 @@ def fit_ppt(img,ppts,tdata,times=None,**kwargs):
     i=0
     for t in times:
         toproject = tdata.loc[tdata.Time==t,["Position X","Position Y"]]
-        SimplePPT.project_ppt(ppts[i],toproject,ax=ax,plot_datapoints=False,alpha_nodes=0,alpha_seg=.05,show=False)
+        simpleppt.project_ppt(ppts[i],toproject,ax=ax,plot_datapoints=False,alpha_nodes=0,alpha_seg=.05,show=False)
         i=i+1
     ax.imshow(img,origin='lower')
     #ax.scatter(mouthpos["Position X"],mouthpos["Position Y"],zorder=2000,s=100,c="red")
--- dyntrack/tools/fit_ppt.py
+++ dyntrack/tools/fit_ppt.py
@@ -2,8 +2,8 @@ from tqdm import tqdm
 import warnings
 import sys
 
-import SimplePPT
-SimplePPT.settings.verbosity=0
+import simpleppt
+simpleppt.settings.verbosity=0
 
 def fit_ppt(tdata,times=None,lam=10,sigma=10,seed=0,**kwargs):
     times=tdata.Time.unique() if times is None else times
@@ -12,6 +12,6 @@ def fit_ppt(tdata,times=None,lam=10,sigma=10,seed=0,**kwargs):
         warnings.simplefilter("ignore")
         for t in tqdm(times,file=sys.stdout, desc="    fitting"):
             tofit = tdata.loc[tdata.Time==t,["Position X","Position Y"]]
-            ppts = ppts + [SimplePPT.ppt(tofit,tofit.shape[0],seed=seed,lam=lam,sigma=sigma,progress=False,**kwargs)]
+            ppts = ppts + [simpleppt.ppt(tofit,tofit.shape[0],seed=seed,lam=lam,sigma=sigma,progress=False,**kwargs)]
         
     return ppts
