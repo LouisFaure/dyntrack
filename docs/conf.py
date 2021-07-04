6c352bd3413c78a5e0ab3625f5df6d19f5ae910a
LouisFaure <louis.faure75@gmail.com>
2021-07-04 13:04:53 +0200

    Added logo rtd

--- docs/conf.py
+++ docs/conf.py
@@ -80,8 +80,34 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-
+html_theme_options = {
+    "titles_only": True,
+    "logo_only": True,
+    # Toc options
+    "collapse_navigation": True,
+    "sticky_navigation": True,
+    "navigation_depth": 4,
+    "includehidden": True,
+    "titles_only": False,
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_context = dict(
+    display_github=True,  # Integrate GitHub
+    github_user="LouisFaure",  # Username
+    github_repo="dyntrack",  # Repo name
+    github_version="main",  # Version
+    conf_py_path="/docs/",  # Path in the checkout to the docs root
+)
+
+html_show_sphinx = False
+html_logo = "_static/dyntrack_logo.svg"
+html_static_path = ["_static"]
+html_extra_path = ["_extra"]
+
+
+def setup(app):
+    app.add_css_file("custom.css")
