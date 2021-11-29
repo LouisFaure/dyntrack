29f4d62f208539f1436e04b866830ed56ab42309
LouisFaure <louis.faure75@gmail.com>
2021-11-29 22:28:50 +0100

    Working compilation during build

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ import pathlib
 from setuptools import setup, find_packages
 import subprocess
 from os import path
+from setuptools.dist import Distribution
 
-this_directory = path.abspath(path.dirname(__file__))
+dir = path.abspath(path.dirname(__file__))
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+with open(path.join(dir, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 from distutils.command.build import build
@@ -21,13 +22,38 @@ class CustomBuild(build):
         subprocess.call(["make", "-C", "vfkm"])
 
 
+class BinaryDistribution(Distribution):
+    """Distribution which always forces a binary package with platform name"""
+
+    def has_ext_modules(foo):
+        return True
+
+
+from setuptools.command.install import install
+
+
+class InstallPlatlib(install):
+    def finalize_options(self):
+        install.finalize_options(self)
+        if self.distribution.has_ext_modules():
+            self.install_lib = self.install_platlib
+
+
+def set_version():
+    head = open(path.join(dir, ".git", "HEAD")).readline()
+    if head.find("dev") != -1:
+        return {
+            "template": "{tag}.dev{ccount}",
+            "dev_template": "{tag}.dev{ccount}",
+            "dirty_template": "{tag}.dev{ccount}",
+        }
+    elif head.find("main") != -1:
+        return {"template": "{tag}", "dev_template": "{tag}", "dirty_template": "{tag}"}
+
+
 setup(
     name="dyntrack",
-    version_config={
-        "template": "{tag}",
-        "dev_template": "{tag}",
-        "dirty_template": "{tag}",
-    },
+    version_config=set_version(),
     setup_requires=["setuptools-git-versioning"],
     description="Python package for the study of particle dynamics from 2D tracks",
     long_description=long_description,
@@ -35,9 +61,8 @@ setup(
     package_dir={"dyntrack": "dyntrack"},
     url="https://github.com/LouisFaure/DynTrack",
     packages=find_packages(),
-    include_package_data=True,
+    include_package_data=False,
     install_requires=requirements,
-    cmdclass={
-        "build": CustomBuild,
-    },
+    cmdclass={"build": CustomBuild, "install": InstallPlatlib},
+    distclass=BinaryDistribution,
 )
